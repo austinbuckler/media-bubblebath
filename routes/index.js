60093b5525ef94a499dce5030389001f5562ddc2
dparkd <d.hanul.park@gmail.com>
2014-08-28 14:33:37 -0700

    First commit

---
+++ routes/index.js
@@ -0,0 +1,104 @@
+var express = require('express');
+var router = express.Router();
+var fs = require('fs');
+var Instagram = require('instagram-node-lib');
+var Flickr = require("node-flickr");
+var twitter = require("node-twitter");
+
+var file = require('../keys.json');
+
+//GET API KEYS
+var keys = {
+  instakey: file.instagram_key,
+  instasecret: file.instagram_secret,
+  twitkey: file.twitter_key,
+  twitsecret:file.twitter_secret,
+  twittoken:file.twitter_token,
+  twittokensecret:file.twitter_token_secret
+};
+
+//SET INSTAGRAM KEY
+Instagram.set('client_id', keys.instakey);
+Instagram.set('client_secret', keys.instasecret);
+
+//SET TWITTER KEYS
+var twitterCli = new twitter.SearchClient(
+  keys.twitkey,
+  keys.twitsecret,
+  keys.twittoken,
+  keys.twittokensecret
+);
+
+//GET FILE
+var instagramJSON = 'public/json/instagram.json';
+var twitterJSON = 'public/json/twitter.json';
+
+router.get('/', function(req, res) {
+  res.render('index', {title:"Media BubbleBath"});
+});
+
+
+router.post('/', function(req, res) {
+
+  var instaData = (JSON.parse(fs.readFileSync('public/json/instagram.json', 'utf8')));
+  var twitData = (JSON.parse(fs.readFileSync('public/json/twitter.json', 'utf8')));
+
+  var searchVariable = req.body.searchquery;
+
+  Instagram.tags.recent({
+    name: searchVariable,
+    complete: function(data){
+      console.log(data);
+
+      fs.writeFile(instagramJSON, JSON.stringify(data, null, 4), function(err) {
+        if(err) {
+          console.log(err);
+        } else {
+          console.log("JSON saved to " + instagramJSON);
+        }
+      });
+
+    }
+
+
+  });
+
+   twitterCli.search({'q':searchVariable}, function(err, twitResult) {
+     fs.writeFile(twitterJSON, JSON.stringify(twitResult, null, 4), function(err) {
+       if(err) {
+         console.log(err);
+       } else {
+         console.log("JSON saved to " + twitterJSON);
+       }
+     });
+   });
+  res.render('instagram', {title:"Media BubbleBath", InstagramData:instaData, TwitterData:twitData});
+  
+});
+
+
+
+module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
